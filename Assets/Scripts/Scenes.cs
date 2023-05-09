48de225e85644a028672a791ac5c75701adf7012
Seden1 <114922611+Seden1@users.noreply.github.com>
2023-05-09 23:48:18 +0300

    B for scene change
    health bar because it gives an error

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -6,16 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
+    int currentLevel = 0;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
-        SceneManager.LoadScene(1,LoadSceneMode.Additive);
+        if(Input.GetKeyDown(KeyCode.B)){
+            LoadLevel();
+        }
+       
         
+    }
+
+    void LoadLevel(){
+             SceneManager.LoadScene(currentLevel+1,LoadSceneMode.Additive);
+             if(currentLevel != 0){
+                SceneManager.UnloadSceneAsync(currentLevel);
+             }    
+        currentLevel += 1;
     }
 }
