353ad5e1aba0a0d7d3ae7e9a24810a202a2b92aa
NurCeylin <ceylincetin03@hotmail.com>
2023-05-29 15:07:40 +0300

    aa

---
+++ Assets/Scripts/Mirror_Color.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror_Color : MonoBehaviour
+{
+    private Light light;
+    private Color color;
+    private float intensity;
+    
+    void Awake(){
+        light = GetComponent<Light>();
+    }
+    
+    void Update(){
+        /*color = Color.green;
+        intensity = 5.0f;
+        light.Em.SetColor("_EmissionColor", color*intensity);*/
+        light.color = Color.green;
+    }
+
+
+   /*[SerializeField] private Material emissiveMaterial;
+   [SerializeField] private Renderer objectToChange;
+
+   private Color color;
+   private float intensity;
+
+
+
+   public void ChangeColor(string green){
+     color = Color.green;
+     intensity = 5.0f;
+     emissiveMaterial.SetColor("_EmissionColor",color*intensity);
+   }*/
+
+
+
+
+}
