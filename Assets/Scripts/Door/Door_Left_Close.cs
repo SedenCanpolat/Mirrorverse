6e1e8111dd346cf94e20e6d5227946f8f392558b
NurCeylin <ceylincetin03@hotmail.com>
2023-05-31 12:12:53 +0300

    kapı açılıyor-kapanamıyor

---
+++ Assets/Scripts/Door/Door_Left_Close.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Left_Close : MonoBehaviour
+{
+    private int control = 0;
+    public GameObject enter;
+    //public GameObject exit;
+    public Transform target;
+    public float t;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if(control == 1){
+           Vector3 a = transform.position;
+           Vector3 b = target.position;
+           transform.position = Vector3.MoveTowards(a,Vector3.Lerp(a,b,t),speed);
+           
+        }
+    }
+
+    void OnTriggerEnter(Collider  other){
+        if(other.gameObject.tag == "Spinning" && control==0){
+          //enter.SetActive(false);
+          control = 1;
+        }   
+    }
+}
